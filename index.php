8aa90e63311cf791c21fd21a50a1e2e96d673e60
Luca Koval <lucakoval@me.com>
2015-12-12 09:26:14 -0500

    Rules
    yee

--- index.php
+++ index.php
@@ -24,53 +24,65 @@
   			<h1 id="jHeader">TSP</h1>
   			<p id="jParagraph">Herp derp.</p>
   		</div>
-  		<div class="panel panel-default">
-	  		<div class="panel-heading">Leaderboard</div>
-	  		<table id="leaderboard" class="table">
-	  			<thead>
-	  				<th>#</th>
-	  				<th>Name</th>
-	  				<th>Distance</th>
-	  			</thead>
-	  			<tbody id="leaderboard">
-	  				<tr>
-	  					<th scope="row">1</th>
-	  					<th>Joshua Gruenstein</th>
-	  					<td>106991</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">2</th>
-	  					<th>Ben Spector</th>
-	  					<td>107031</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">3</th>
-	  					<th>Jonathan Mendelson</th>
-	  					<td>115095</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">4</th>
-	  					<th>Henry Wildermuth</th>
-	  					<td>115581</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">5</th>
-	  					<th>Nick Keirstead</th>
-	  					<td>118583</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">6</th>
-	  					<th>Aman Sanger</th>
-	  					<td>119889</td>
-	  				</tr>
-	  				<tr>
-	  					<th scope="row">7</th>
-	  					<th>Nicholas Carrero</th>
-	  					<td>533372</td>
-	  				</tr>
-	  			</tbody>
-	  		</table>
-	  	</div>
+  		<div class="row">
+  			<div class="col-sm-4">
+  				<div class="panel panel-primary">
+  					<div class="panel-heading">
+  						<h3 class="panel-title">Rules</h3>
+  					</div>
+  					<div class="panel-body">These are the rules.</div>
+  				</div>
+  			</div>
+  			<div class="col-sm-8">
+		  		<div id="leaderboardContainer" class="panel panel-default">
+			  		<div class="panel-heading">Leaderboard</div>
+			  		<table id="leaderboard" class="table">
+			  			<thead>
+			  				<th>#</th>
+			  				<th>Name</th>
+			  				<th>Distance</th>
+			  			</thead>
+			  			<tbody id="leaderboard">
+			  				<tr>
+			  					<th scope="row">1</th>
+			  					<th>Joshua Gruenstein</th>
+			  					<td>106991</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">2</th>
+			  					<th>Ben Spector</th>
+			  					<td>107031</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">3</th>
+			  					<th>Jonathan Mendelson</th>
+			  					<td>115095</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">4</th>
+			  					<th>Henry Wildermuth</th>
+			  					<td>115581</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">5</th>
+			  					<th>Nick Keirstead</th>
+			  					<td>118583</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">6</th>
+			  					<th>Aman Sanger</th>
+			  					<td>119889</td>
+			  				</tr>
+			  				<tr>
+			  					<th scope="row">7</th>
+			  					<th>Nicholas Carrero</th>
+			  					<td>533372</td>
+			  				</tr>
+			  			</tbody>
+			  		</table>
+			  	</div>
+			</div>
+		</div>
   	</div>
 
     <script src="lib/jquery.min.js"></script>
