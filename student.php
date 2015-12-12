c160ee5da280154115ccc552de3a6e17ac66eb83
Luca Koval <lucakoval@me.com>
2015-12-12 10:04:45 -0500

    color scheme
    yee

--- student.php
+++ student.php
@@ -43,12 +43,6 @@
 
 			</div>
 		</div>
-		<nav>
-			<ul class="pager">
-				<li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
-				<li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
-			</ul>
-		</nav>
 	</div>
 
 	<script src="lib/jquery.min.js"></script>
