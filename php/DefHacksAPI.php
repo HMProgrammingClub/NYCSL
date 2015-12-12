ca968dc6967bdeae38f7c9ccdcedd9a9c0305b75
truell20 <truell20@gmail.com>
2015-12-12 15:16:35 -0500

    Real sanatize

--- php/DefHacksAPI.php
+++ php/DefHacksAPI.php
@@ -26,14 +26,14 @@ class DefHacksAPI extends API
 	}
 
 	private function select($sql) {
-		$sql = mysqli_escape_string($sql);
+		$sql = $this->mysqli->escape_string($sql);
 		$res = mysqli_query($this->mysqli, $sql);
 		if($res) return mysqli_fetch_array($res, MYSQLI_ASSOC);
 		else return NULL;
 	}
 
 	private function selectMultiple($sql) {
-		$sql = mysqli_escape_string($sql);
+		$sql = $this->mysqli->escape_string($sql);
 		$res = mysqli_query($this->mysqli, $sql);
 		$finalArray = array();
 
@@ -45,7 +45,7 @@ class DefHacksAPI extends API
 	}
 
 	private function insert($sql) {
-		$sql = mysqli_escape_string($sql);
+		$sql = $this->mysqli->escape_string($sql);
 		mysqli_query($this->mysqli, $sql);
 	}
 
