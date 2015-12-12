b7a4e01d59641206487cd028e16dc1107f55e559
joshuagruenstein <joshuagruenstein@gmail.com>
2015-12-12 08:37:25 -0500

    lib and bootstrap template

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title></title>
+
+	<link href="lib/bootstrap.min.css" rel="stylesheet">
+
+	<link href="style/general.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  	<div class="container">
+  		<?php include 'includes/navbar.php'; ?>
+  		<div class="jumbotron">
+  			<h1>Navbar example</h1>
+  			<p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+  			<p>
+  				<a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+  			</p>
+  		</div>
+
+  	</div>
+
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/bootstrap.min.js"></script>
+</body>
+</html>
