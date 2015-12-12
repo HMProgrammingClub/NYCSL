6d949c47f5269ff93069d9bcfb75a31849051d7c
Luca Koval <lucakoval@me.com>
2015-12-12 12:10:45 -0500

    Finished footer
    yee

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,14 @@
-<div id="footer">
-	<div class="navbar navbar-inner  navbar-fixed-bottom" id="navCont">
-		<nav>
+<footer class="footer">
+	<div class="container">
+		<div id="footer">
 			<ul class="pager">
 				<li class="previous" id="backButton"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
 				<li><a href="#" data-toggle="modal" data-target="#aboutModal">About</a></li>
 				<li class="next" id="nextButton"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
 			</ul>
-		</nav>
+		</div>
 	</div>
-</div>
+</footer>
 
 <div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog" role="document">
@@ -20,9 +20,10 @@
 			<div class="modal-body">
 				<p>NYCSL, or the New York Computer Science League, is a computer programming competition website.  It was created by four programmers, Josh Gruenstein, Luca Koval, Ben Spector, and Michael Truell, during the defhacks hackathon for New York City high school students.</p>
 				<p>The purpose of NYCSL is to bring NYC highschool programmers together to create and share ideas through competition.  Each month, a new competition with a new problem is created and posted on the website.  Programmers in NYCSL have one month to upload a solution to the problem.  Their solutions are graded and then put up on a leaderboard with other scores from other students from the same school.<p>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
 			</div>
 		</div>
 	</div>
