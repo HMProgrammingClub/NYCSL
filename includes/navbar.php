87a2b5be735208b9ef19be63f14203ee356ec47c
joshuagruenstein <joshuagruenstein@gmail.com>
2015-12-13 10:53:36 -0500

    material design

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/"><img height="20px" src="img/Logo.png"></a>
+			<a class="navbar-brand" href="#">NYCSL</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
