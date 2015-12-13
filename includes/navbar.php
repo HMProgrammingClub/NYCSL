62224bbfeb6ceda25594ac75436d9f0e33385971
joshuagruenstein <joshuagruenstein@gmail.com>
2015-12-13 11:29:42 -0500

    navbar border radius

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default" id="navvy">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">NYCSL</a>
+			<a class="navbar-brand" href="/">NYCSL</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
