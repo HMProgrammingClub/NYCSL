90ee193353e18625b8ec9aad225d985bd44fb148
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-01 01:55:58 -0500

    about updates

--- includes/footer.php
+++ includes/footer.php
@@ -15,15 +15,24 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="myModalLabel">About NYCSL</h4>
+				<h4 class="modal-title" id="myModalLabel">New York Computer Science League</h4>
 			</div>
 			<div class="modal-body">
-				<p>NYCSL, or the New York Computer Science League, is a programming competition website created by programmers Josh Gruenstein, Luca Koval, Ben Spector, and Michael Truell during the defhacks("Winter",2015) hackathon.</p>
-				<p>Each month, a new problem is posted on the website.  Each programmer in NYCSL (any high school student in the New York is eligible) has one month to upload a solution to the problem.  Their solutions are instantly graded and put up on both global and school-wide leaderboards.<p>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
+				<p>NYCSL, or the New York Computer Science League, is a programming competition created for New York high schools students to compete against each-other to solve algorithmic computer science problems.</p>
+				<p>Each month at midnight a new challenge is posted.  Each programmer in NYCSL has one month to upload as many solutions as they like; only the top score is counted.  Solutions are instantly graded and put up on both global and school-wide leaderboards.<p>
+				<p>Currently the following schools are supported. If you'd like your school to be added, please contact us.
+					<ul>
+						<li>Dalton</li>
+						<li>Horace Mann</li>
+						<li>Stuyvesant</li>
+						<li>Fieldston</li>
+						<li>Trinity</li>
+						<li>Bronx Science</li>						
+					</ul>
+				
+				</p>
+				<p>NYCSL.io was created by programmers <a href="https://github.com/joshuagruenstein">Josh Gruenstein</a>, <a href="https://github.com/G4cool">Luca Koval</a>, <a href="https://github.com/Sydriax">Ben Spector</a>, and <a href="https://github.com/truell20">Michael Truell</a> during the defhacks("Winter",2015) hackathon at Facebook NY.  The code is available on Github <a href="https://github.com/BackPropagation/NYCSL">here</a>.  You may contact us at <a href="mailto:contact@nycsl.io">contact@nycsl.io</a>.
+</p>.</p>
 			</div>
 		</div>
 	</div>
