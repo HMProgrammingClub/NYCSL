9768011eeba9e583d71c4eda4a9c4f10a70f1b70
Michael Truell <truell20@gmail.com>
2016-01-01 02:03:44 -0500

    Added email address

--- php/MailOperation.php
+++ php/MailOperation.php
@@ -8,5 +8,5 @@
 	mail($email, 
 		"NYCSL.io Registration Confirmation", 
 		"Click <a href='nycsl.io/verify.php?code={$verificationCode}&userID={$userID}'>here</a> to confirm registration for $name at NYCSL.io. If you did not register, ignore this message.",
-		"Content-Type: text/html; charset=ISO-8859-1\r\n");
+		"From: noreply@nycsl.io\r\n Reply-To: noreply@nycsl.io\r\nContent-Type: text/html; charset=ISO-8859-1\r\n");
 ?>
