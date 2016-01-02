db402d24e646259ca4b47ee4f792a0e1151a92f4
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-02 13:20:17 -0500

    added more headers

--- php/MailOperation.php
+++ php/MailOperation.php
@@ -5,8 +5,10 @@
 	$verificationCode = $argv[3];
 	$name = $argv[4];
 
+	$headers = "From: NYCSL <noreply@nycsl.io>\r\nReply-To: NYCSL <noreply@nycsl.io>\r\nX-Priority: 3\r\nX-Mailer: PHP". phpversion() ."\r\nContent-Type: text/html; charset=ISO-8859-1\r\nOrganization: NYCSL\r\nMIME-Version: 1.0\r\n";
+
 	mail($email, 
-		"NYCSL.io Registration Confirmation", 
+		"NYCSL.io Email Confirmation", 
 		"Click <a href='http://nycsl.io/verify.php?code={$verificationCode}&userID={$userID}'>here</a> to confirm registration for $name at NYCSL.io. If you did not register, ignore this message.",
-		"From: noreply@nycsl.io\r\nReply-To: noreply@nycsl.io\r\nX-Mailer: PHP/".phpversion()."\r\nContent-Type: text/html; charset=ISO-8859-1\r\n");
+		$headers);
 ?>
