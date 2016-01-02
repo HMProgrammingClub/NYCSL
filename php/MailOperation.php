552d65163c23c09bea0857945c0947e16d918410
Michael Truell <truell20@gmail.com>
2016-01-02 14:06:39 -0500

    mail seems to be working

--- php/MailOperation.php
+++ php/MailOperation.php
@@ -10,9 +10,5 @@
 	mail($email, 
 		"NYCSL.io Email Confirmation", 
 		"Click <a href='http://nycsl.io/verify.php?code={$verificationCode}&userID={$userID}'>here</a> to confirm registration for $name at NYCSL.io. If you did not register, ignore this message.",
-<<<<<<< HEAD
 		$headers);
-=======
-		"From: noreply@nycsl.io\r\nReply-To: noreply@nycsl.io\r\nX-Mailer: PHP/".phpversion()."\r\nContent-Type: text/html; charset=ISO-8859-1\r\n");
->>>>>>> parent of 18bc330... Temporarily took out email verification
 ?>
