d90090679509b0dbe7f9d16490b5f980f7904f6d
truell20 <truell20@gmail.com>
2016-01-05 22:21:31 -0500

    Recover Password Initial implementation

--- script/recover.js
+++ script/recover.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	var userID = getGET("userID");
-	var resetCode = getGET("resetCode");
+	var recoveryCode = getGET("code");
 
 	$("#recoButton").click(function() {
 		var pass = ("#passField").val();
-		// do stuff
+		var result = recoverEmail(userID, recoveryCode, pass)
+		if(result == null) window.location.href = "index.php?didNotRecover=1"
+		else window.location.href = "index.php?didRecover=1"
 	})
 })
