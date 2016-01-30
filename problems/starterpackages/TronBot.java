2186997a89ac4019ab44e6db329ceb3be0c0b8c0
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-30 17:49:41 -0500

    not sure what's in this commit

--- problems/starterpackages/TronBot.java
+++ problems/starterpackages/TronBot.java
@@ -1,20 +1,17 @@
 public class TronBot {
 	public static void main(String[] args) {
-		// Initialize the Tron library
-		Tron tron = new Tron();
-
 		// Execute loop forever (or until game ends)
 		while (true) {
 			/* Get an integer map of the field. Each int
 			 * can either be Tron.Tile.EMPTY, Tron.Tile.PLAYER1, 
 			 * Tron.Tile.PLAYER2, Tron.Tile.TAKEN_BY_PLAYER1, or
 			 * Tron.Tile.TAKEN_BY_PLAYER2.   */
-			int[][] gameMap = tron.getMap();
+			int[][] gameMap = Tron.getMap();
 
 			/* Send your move.  This can be Tron.Direction.NORTH,
 			 * Tron.Direction.SOUTH, Tron.Direction.EAST, or
 			 * Tron.Direction.WEST.          */
-			tron.sendMove(Tron.Direction.NORTH);
+			Tron.sendMove(Tron.Direction.NORTH);
 		}
 	}
 
