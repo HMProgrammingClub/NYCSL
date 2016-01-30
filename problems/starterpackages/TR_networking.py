00176612aff3355837888747688502ec5f254d9a
truell20 <truell20@gmail.com>
2016-01-30 13:36:18 -0500

    Enums

--- problems/starterpackages/TR_networking.py
+++ problems/starterpackages/TR_networking.py
@@ -1,4 +1,18 @@
 import sys
+from enum import Enum
+
+class Direction(Enum):
+	north = 0
+	east = 1
+	south = 2
+	west = 3
+
+class Tile(Enum):
+	empty = 0
+	player1 = 1
+	player2 = 2
+	takenByPlayer1 = 3
+	takenByPlayer2 = 4
 
 class Networker:
 	def deserializeMap(self, mapString):
@@ -6,7 +20,7 @@ class Networker:
 		return [tiles[a*16 : (a+1)*16] for a in range(16)]
 
 	def getMap(self):
-		return deserializeMap(sys.stdin.readline().rstrip("\n"))
+		return self.deserializeMap(sys.stdin.readline().rstrip("\n"))
 
 	def sendMove(self, move):
 		sys.stdout.write(str(move) + "\n")
