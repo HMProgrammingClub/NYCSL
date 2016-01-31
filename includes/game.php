b7176e1e31fe08de8bb126c208689294c48b8470
joshuagruenstein <joshuagruenstein@gmail.com>
2016-01-30 20:19:33 -0500

    game modal

---
+++ includes/game.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="gameModal" tabindex="-1" role="dialog" aria-labelledby="gameModal">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="gameModalLabel"><span style="color:blue">Person A</span> vs <span style="color:red">Person B</span></h4>
+			</div>
+			<div class="modal-body">
+				<canvas></canvas>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary">Upload .trn File</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
