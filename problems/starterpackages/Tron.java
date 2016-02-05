335c8e4471db3189d823c5944c34307ce2461bdb
truell20 <truell20@gmail.com>
2016-02-05 17:11:22 -0500

    Fixed java starter package error

--- problems/starterpackages/Tron.java
+++ problems/starterpackages/Tron.java
@@ -1,6 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Tron {
+
+	//We can't print to console, because that channel is used for IO with the environment.
+	//We therefore create a PrintWriter and output to that. Use "OutputDebug" rather than "System.out.print".
+	public static PrintWriter debug;
+	public static <T> void outputDebug(T s) {
+		if(debug == null) {
+			try {
+				debug = new PrintWriter("debug-" + (System.currentTimeMillis() / 1000) + ".log"); //Cast to seconds.
+			} catch(Exception e) {
+				System.out.println("Debug file could not be open.");
+				System.exit(1);
+			}
+		}
+		debug.print(s);
+		debug.flush();
+	}
 
 	public static enum Direction {
 		NORTH, EAST, SOUTH, WEST
@@ -12,7 +29,7 @@ public class Tron {
 	}
 
 	private static int[][] deserializeMap(String mapString) {
-		String[] tiles = mapString.split(" ");
+		String[] tiles = mapString.trim().split(" ");
 		int[] tileVals = new int[tiles.length];
 		for (int i=0; i<tiles.length; i++) tileVals[i] = Integer.parseInt(tiles[i].trim());
 
@@ -40,8 +57,11 @@ public class Tron {
     }
 
 	public static int[][] getMap() {
-		String message = getString();
-		if(message.equals("KILL")) System.exit(0);
+		String message = getString().trim();
+		if(message.equals("KILL")) {
+			outputDebug("exit");
+			System.exit(0);
+		}
 		return deserializeMap(message);
 	}
 
