dcedb0a68763fc999588f23c70b8380fdf7cc133
truell20 <truell20@gmail.com>
2016-02-06 11:45:30 -0500

    Fixed c++ logging

--- problems/starterpackages/TronBot.cpp
+++ problems/starterpackages/TronBot.cpp
@@ -2,13 +2,12 @@
 
 //We can't print to console, because that channel is used for IO with the environment.
 //We therefore create a fstream and output to that. Use "debug << " rather than "std::cout << ".
-std::ofstream debug;
 
 int main() {
-	debug = std::ofstream("debug-" + std::to_string(time(NULL)) + ".log");
 	
 	// Execute loop forever (or until game ends)
 	while (true) {
+		log("c++ test");
 		/* Get an integer map of the field. Each int
 		 * can either be Tron.Tile.EMPTY, Tron.Tile.PLAYER1, 
 		 * Tron.Tile.PLAYER2, Tron.Tile.TAKEN_BY_PLAYER1, or
