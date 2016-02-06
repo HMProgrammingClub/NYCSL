257bc7cf8f55f77a412e16d89023dd3edebb3f44
truell20 <truell20@gmail.com>
2016-02-06 11:45:40 -0500

    Took out Henry's bot

--- problems/starterpackages/TronBot.py
+++ problems/starterpackages/TronBot.py
@@ -3,31 +3,9 @@ from Tron import *
 #We therefore create a file and output to that. Use debug rather than print.
 
 networker = Networker()
-index = 0
-direction = 0
-log("test")
-
-def findMe(gameMap):
-	for i in range(len(gameMap)):
-		for j in range(len(gameMap[i])):
-			if (gameMap[i][j] == 1):
-				return (i, j)
-
-gameMap = networker.getMap()
-row, col = findMe(gameMap)
-
-log( str(row) + ", " +str(col))
-
-if row == 0: direction = 3
-else: direction = 0
-
-log(direction)
-
-networker.sendMove(direction)
-
 while True:
 	gameMap = networker.getMap()
-	log(str(findMe(gameMap)))
+	log("python test")
 
 	# actually send it
-	networker.sendMove(direction)
+	networker.sendMove(north)
