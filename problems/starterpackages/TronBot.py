5974453ab38ff951e464f8de0cd743f926ffdb4b
truell20 <truell20@gmail.com>
2016-02-06 18:05:16 -0500

    Update python bot comments

--- problems/starterpackages/TronBot.py
+++ problems/starterpackages/TronBot.py
@@ -1,11 +1,15 @@
 from Tron import *
-#We can't print to console, because that channel is used for IO with the environment.
-#We therefore create a file and output to that. Use debug rather than print.
 
 networker = Networker()
 while True:
+	# Get the current map. It is a 2d list of integers.
+	# Each int can be the value of one of these variables: north, south, east, west
 	gameMap = networker.getMap()
+
+	# We can't print to console, because that channel is used for IO with the environment.
+	# We therefore print to either debug1.log or debug2.log. The first is for player1, the second for layer two
+	# Use log() rather than print.
 	log("python test")
 
-	# actually send it
+	# Send your move
 	networker.sendMove(north)
