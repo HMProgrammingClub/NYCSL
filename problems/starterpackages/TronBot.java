eff09f4441cf638c52a207d4e9fcdcc8c132c9ef
truell20 <truell20@gmail.com>
2016-02-06 18:29:29 -0500

    Comment starter packages

--- problems/starterpackages/TronBot.java
+++ problems/starterpackages/TronBot.java
@@ -6,6 +6,11 @@ public class TronBot {
 		// Call this to initialize networking and the debug file
 		Tron.init();
 
+		// We can't print to console, because that channel is used for IO with the environment.
+		// We therefore print to either debug1.log or debug2.log. The first is for player1, the second for layer two
+		// Use log() rather than print.
+		Tron.log("This is a java bot");
+
 		// Execute loop forever (or until game ends)
 		while (true) {
 			/* Get an integer map of the field. Each int
@@ -14,7 +19,14 @@ public class TronBot {
 			 * Tron.Tile.TAKEN_BY_OPPONENT.   */
 			ArrayList<ArrayList<Tron.Tile>> gameMap = Tron.getMap();
 
-			Tron.log("java test");
+			// Let's find our current position and log it
+			for(int y = 0; y < gameMap.size(); y++) {
+				for(int x = 0; x < gameMap.size(); x++) {
+					if(gameMap.get(y).get(x) == Tron.Tile.ME) {
+						Tron.log("position: " + x + ", " + y);
+					}
+				}
+			}
 
 			/* Send your move.  This can be Tron.Direction.NORTH,
 			 * Tron.Direction.SOUTH, Tron.Direction.EAST, or
