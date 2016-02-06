869ce5baa6e2bbee3700bb422b19baf205cd20dc
truell20 <truell20@gmail.com>
2016-02-05 22:24:05 -0500

    Starter packages and environment fixed

--- problems/starterpackages/Tron_Environment.py
+++ problems/starterpackages/Tron_Environment.py
@@ -1,8 +1,64 @@
-from TR_environment_networking import Networker
 from enum import Enum
 import time
 import copy
 import sys
+import subprocess
+from threading import Thread
+
+def monitorFile(connection, queue):
+	while True:
+		try:
+			line = connection.readline()
+		except:
+			break
+
+		if not line:
+			queue.append(None)
+			break
+		line = line.rstrip("\n")
+		queue.append(line)
+
+class Networker:
+	def __init__(self):
+		self.processes = []
+		self.stdoutQueues = []
+		self.stderrQueues = []
+
+	def startPlayer(self, command):
+		self.processes.append(subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, shell=True))
+
+		self.stdoutQueues.append([])
+		self.stderrQueues.append([])
+		
+		stdoutMonitor = Thread(target=monitorFile, args=(self.processes[-1].stdout, self.stdoutQueues[-1]))
+		stdoutMonitor.daemon = True
+		stdoutMonitor.start()
+
+		stderrMonitor = Thread(target=monitorFile, args=(self.processes[-1].stderr, self.stderrQueues[-1]))
+		stderrMonitor.daemon = True
+		stderrMonitor.start()
+		
+	def serializeMap(self, map, isSecond):
+		returnString = ""
+		for row in map:
+			for tile in row:
+				returnString += str(tile-1 if tile == 2 or tile == 4 else (tile+1 if tile != 0 else tile)) + " "
+		return returnString
+		
+	def frameNetworking(self, map, isSecond):
+		self.processes[isSecond].stdin.write(self.serializeMap(map, isSecond) + "\n")
+		self.processes[isSecond].stdin.flush()
+
+		# Return move
+		while len(self.stdoutQueues[isSecond]) == 0:
+			time.sleep(0.01)
+		return int(self.stdoutQueues[isSecond].pop())
+
+	def killAll(self):
+		for a in range(len(self.processes)):
+			self.processes[a].stdin.write("KILL\n")
+			self.processes[a].stdin.flush()
+			self.processes[a].kill()
 
 class Direction(Enum):
 	north = 0
@@ -62,8 +118,10 @@ while isDone == False:
 			elif move == Direction.west.value: positions[a].x -= 1
 
 			# check if legitimate move
-			if positions[a].x >= width or positions[a].y >= height or positions[a].x < 0 or positions[a].y < 0 or gameMap[positions[a].y][positions[a].x] == Tile.takenByPlayer1.value or gameMap[positions[a].y][positions[a].x] == Tile.takenByPlayer2.value:
-				print("Player " + str(a+1) + " moved off of the map!")
+			if positions[a].x >= width or positions[a].y >= height or positions[a].x < 0 or positions[a].y < 0 or gameMap[positions[a].y][positions[a].x] != Tile.empty.value:
+				if positions[a].x >= width or positions[a].y >= height or positions[a].x < 0 or positions[a].y < 0: print("Player " + str(a+1) + " fell off the map!")
+				elif gameMap[positions[a].y][positions[a].x] == Tile.player1.value or gameMap[positions[a].y][positions[a].x] == Tile.player1.value: print("Player " + str(a+1) + " collided with another player!")
+				else: print("Player " + str(a+1) + " collide with a tile that has already been taken!")
 				winner = 1 + (0 if a == 1 else 1)
 
 				if isDone == True: isTied = True
