7195296ec8437eec49c996992f8113ecdd5f81fd
Michael Truell <truell20@gmail.com>
2016-06-03 18:28:22 -0400

    Delete TR.py

--- problems/cron/TR.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-
-urllib.request.urlopen("https://slack.com/api/chat.postMessage?token=xoxp-9885641827-13138763941-31528832983-8b62a01f6d&channel=programming_electrics&text=hello")
