c65306f78f1eb97714fd2086d20ff781faf78c3a
Michael Truell <michael_truell@horacemann.org>
2016-06-04 12:57:06 -0400

    Make py starterpackage more like java/c++ one

--- problems/starterpackages/SteinerStarter.py
+++ problems/starterpackages/SteinerStarter.py
@@ -11,6 +11,7 @@ class Edge:
 		self.p1 = p1
 		self.p2 = p2
 
+# Gets a problem from a file as an list of points.
 def getProblem(filename):
 	pts = []
 	with open(filename, 'r') as input:
@@ -19,14 +20,17 @@ def getProblem(filename):
 			pts.append(Point(float(l[0]), float(l[1])))
 	return pts
 
+# Outputs a list of edges to file "out.txt" for submission.
 def outputSolutionsToFile(edges):
 	f = open("out.txt", 'w')
 	for a in edges:
 		f.write(str(a.p1.x) + ' ' + str(a.p1.y) + ' ' + str(a.p2.x) + ' ' + str(a.p2.y) + '\n')
 
-pts = [] # Where you will find the input points.
+pts = getProblem("st.txt") # Where you will find the input points.
 edges = [] # Edges should be added to this.
 		
 # Your code here. This sample code just connects the points in the order that they are given:
 for a in range(1, len(pts)):
 	edges.append(Edge(pts[a-1], pts[a]))
+
+outputSolutionsToFile(edges)
