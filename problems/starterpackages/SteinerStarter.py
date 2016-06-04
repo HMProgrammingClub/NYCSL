2bb148ebffb31f30829a0b9c82570c958fa162e4
Michael Truell <michael_truell@horacemann.org>
2016-06-04 12:43:48 -0400

    ST file io to functions

--- problems/starterpackages/SteinerStarter.py
+++ problems/starterpackages/SteinerStarter.py
@@ -10,21 +10,23 @@ class Edge:
 	def __init__(self, p1, p2):
 		self.p1 = p1
 		self.p2 = p2
-		
+
+def getProblem(filename):
+	pts = []
+	with open(filename, 'r') as input:
+		for line in input:
+			l = line.split(' ')
+			pts.append(Point(float(l[0]), float(l[1])))
+	return pts
+
+def outputSolutionsToFile(edges):
+	f = open("out.txt", 'w')
+	for a in edges:
+		f.write(str(a.p1.x) + ' ' + str(a.p1.y) + ' ' + str(a.p2.x) + ' ' + str(a.p2.y) + '\n')
+
 pts = [] # Where you will find the input points.
 edges = [] # Edges should be added to this.
-
-# Gets the input points from st.txt for you.
-with open('st.txt', 'r') as input:
-	for line in input:
-		l = line.split(' ')
-		pts.append(Point(float(l[0]), float(l[1])))
 		
 # Your code here. This sample code just connects the points in the order that they are given:
 for a in range(1, len(pts)):
 	edges.append(Edge(pts[a-1], pts[a]))
-
-# Outputs your edges to file out.txt for submission.
-f = open('out.txt', 'w')
-for a in edges:
-	f.write(str(a.p1.x) + ' ' + str(a.p1.y) + ' ' + str(a.p2.x) + ' ' + str(a.p2.y) + '\n')
